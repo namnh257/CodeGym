00acad4714cd225b98ab743315f2c561b0623c0f
namnh257 <namnh257@gmail.com>
2023-04-06 17:05:52 +0700

    DienTichHinhChuNhat
    Sử dụng toán tử

---
+++ DienTichHinhChuNhat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DienTichHinhChuNhat
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            float width;
+            float height;
+
+            Console.Write("Nhap chieu rong: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu dai: ");
+            height = float.Parse(Console.ReadLine());
+
+            float area = width * height ;
+            Console.WriteLine ("Dien tich hinh chu nhat la :" + area);
+
+        }
+    }
+}
