da24ae76cbad471459c76007fff22f19ab151047
namnh257 <namnh257@gmail.com>
2023-04-06 17:11:03 +0700

    HienThiLoiChao
    Hiển thị lời chào

---
+++ HienThiLoiChao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Nhap ten cua ban: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Xin chao: " + yourName);
+        }
+    }
+}
