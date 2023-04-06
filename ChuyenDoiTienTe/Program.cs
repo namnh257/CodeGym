22c3485c1c1735fdc254d665c319ffe8ab63f802
namnh257 <namnh257@gmail.com>
2023-04-06 17:17:55 +0700

    Chuyen Doi Tien Te
    Ứng dụng chuyển đổi tiền tệ

---
+++ ChuyenDoiTienTe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float rate;
+            Console.WriteLine("Vui long nhap ti gia: ");
+            rate = float.Parse(Console.ReadLine());
+            int usd;
+            Console.WriteLine("Vui long nhap so luong ban muon quy doi: ");
+            usd = int.Parse(Console.ReadLine());
+            float quyDoi;
+            quyDoi = rate*usd;
+            Console.WriteLine("So tien cua ban quy doi ra VND bang: " + quyDoi);
+
+        }
+    }
+}
