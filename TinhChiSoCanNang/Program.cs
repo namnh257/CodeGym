329f445dcba9f8aa7ec1f8bca9b5f19a81909222
namnh257 <namnh257@gmail.com>
2023-04-07 10:47:10 +0700

    Tinh Chi So Can Nang

---
+++ TinhChiSoCanNang/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TinhChiSoCanNang
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {   
+            double height;
+            Console.Write ("Nhap chieu cao (cm): ");
+            height = double.Parse(Console.ReadLine())/100;
+
+            double weight;
+            Console.Write ("Nhap can nang (kg): ");
+            weight = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (height*height);
+            
+            Console.WriteLine ("BMI: " + bmi);
+            if (bmi < 18.5)
+                {
+                    Console.WriteLine("Underweight");
+                }
+            else if (bmi < 25)
+                {
+                    Console.WriteLine("Normal");
+                }
+            else if (bmi < 30)
+                {
+                    Console.WriteLine("Overweight");
+                }
+            else
+                {
+                    Console.WriteLine("Obese");
+                }
+        }
+    }
+}
